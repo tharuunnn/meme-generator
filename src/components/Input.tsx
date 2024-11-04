4c8c49e8bc5d7c251b4b2d6a4233f1269889d575
tharuunnn <tharun220804@gmail.com>
2024-11-04 12:49:14 +0530

    meme-ipsection-done

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,30 @@
 const Input: React.FC = () => {
   return(
-    <div>
-      <label>Top text</label>
-      <input type="text" placeholder="Shut up"/>
-      <label>Bottom text</label>
-      <input type="text" placeholder="and take my money"/>
+  <div className="meme-input-container">
+    <div className="ip-container">
+      <div className="top-ip-container">
+        <div>
+          <label className="toptext">Top text</label>
+        </div>
+        <div>
+          <input type="text" placeholder="Shut up"  className="top-input"/>
+        </div>
+      </div>
+      <div className="bottom-ip-container">
+        <div>
+          <label className="bottomtext">Bottom text</label>
+        </div>
+        <div>
+          <input type="text" placeholder="and take my money" className="bottom-input"/>
+        </div>
+     </div>
     </div>
+    <div className="new-meme-button-container">
+      <button className="new-meme-button">
+        Get a new meme image 🖼
+      </button>
+    </div>
+  </div>
   )
 }
 
